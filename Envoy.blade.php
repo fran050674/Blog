5026d3f5fa3978f5d9fd1c0e7a154c7c4acd5b11
francisco <franciscojavier.andgo@gmail.com>
2018-01-02 13:17:33 +0100

    Fith commit

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,6 +1,7 @@
 @servers(['blog' => 'admin@37.187.109.143'])
 
 @task('deploy', ['on' => 'blog'])
-    cd /home/admin/projects
+    cd /home/admin/projects/blog
+    git pull origin master
     composer install
 @endtask
