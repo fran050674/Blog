cecd8f6d79816089f089c2acf89d825cca5d9893
francisco <franciscojavier.andgo@gmail.com>
2018-01-02 13:06:26 +0100

    Third commnit

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,7 +1,7 @@
 @servers(['blog' => 'admin@37.187.109.143'])
 
 @task('deploy', ['on' => 'blog'])
-    cd /home/admin/projects/blog
+    cd /home/admin/projects
     git pull origin master
     composer install
 @endtask
